b5efa1dc1030b224f9bc283f9d22d4167143ca06
leemr <think-roon@naver.com>
2022-06-04 08:08:21 +0900

    실행시간 측정 로깅 AOP

---
+++ src/main/java/pratice/roon/misedirt/advice/ExecutionTimeLogger.java
@@ -0,0 +1,34 @@
+package pratice.roon.misedirt.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@Aspect
+public class ExecutionTimeLogger {
+    @Around("@annotation(LogExecutionTime)")
+    public void logAround(ProceedingJoinPoint joinPoint) {
+        long start = System.nanoTime();
+
+        try {
+            joinPoint.proceed();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        long end = System.nanoTime();
+
+        //TODO : reflection이 비싼 연산이라던데 실행시간 측정하다가 성능에 영향 주게 되는건가
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        log.info("[execution time] " + methodName + "= " + (end - start) / 1_000_000 + " ms");
+    }
+}
