b68f11cae17d421476f80e9a9abaec2f66f3a841
auddl0756 <cjddl789123@naver.com>
2022-07-10 15:56:25 +0900

    회원 가입 기능

---
+++ src/main/java/pratice/roon/misedirt/auth/AuthController.java
@@ -0,0 +1,41 @@
+package pratice.roon.misedirt.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import pratice.roon.misedirt.auth.dto.AuthDTO;
+import pratice.roon.misedirt.auth.dto.SignUpDTO;
+import pratice.roon.misedirt.auth.service.MemberService;
+
+@Controller
+public class AuthController {
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    // 스프링 시큐리티가 폼 로그인 과정을 처리해서 로그인 처리 로직을 직접 작성하면 안됨
+//    @PostMapping("/login")
+//    public String processLogin(@RequestBody AuthDTO authDTO){
+//        System.out.println(authDTO);
+//        return "/mise/main";
+//    }
+
+    @GetMapping("/enroll")
+    public String enroll(){ return "enroll";}
+
+    @PostMapping("/enroll")
+    public RedirectView enroll(SignUpDTO signUpDTO){
+        System.out.println(signUpDTO);
+
+        memberService.enrollUser(signUpDTO.getUsername(), signUpDTO.getPassword());
+
+        return new RedirectView("/");
+    }
+
+}
